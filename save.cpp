27d39fa3e35f9edb93b29c9970472ce20b69a93b
sunilmaurya <sunilmaurya@lidolearning.com>
2020-07-11 19:02:54 +0530

    added template for competetive programming

---
+++ save.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+#define SPEED  ios::sync_with_stdio(false);cin.tie(0); cout.tie(0);
+#define pii pair<int,int>
+using namespace std;
+typedef long long int ll;
+ll m=1000000007;
+int main()
+{
+    SPEED;
+    return 0;
+}
