d40d71a28bd682f32699194349db30144eedb632
sunil <sunilmaurya@lidolearning.com>
2020-10-13 22:21:28 +0530

    added question link

--- leetcode/two pointer/27_Remove_Element.cpp
+++ leetcode/two pointer/27_Remove_Element.cpp
@@ -1,6 +1,19 @@
 // question link
-//https://leetcode.com/problems/remove-element/
+// https://leetcode.com/problems/remove-element/
 
+// Given an array nums and a value val, remove all instances of that value in-place and return the new length.
+
+// Do not allocate extra space for another array, you must do this by modifying the input array in-place with O(1) extra memory.
+
+// The order of elements can be changed. It doesn't matter what you leave beyond the new length.
+
+// Example 1:
+
+// Given nums = [3,2,2,3], val = 3,
+
+// Your function should return length = 2, with the first two elements of nums being 2.
+
+// It doesn't matter what you leave beyond the returned length.
 #include <bits/stdc++.h>
 #define SPEED  ios::sync_with_stdio(false);cin.tie(0); cout.tie(0);
 #define pii pair<int,int>
