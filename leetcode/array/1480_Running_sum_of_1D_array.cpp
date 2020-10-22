32891d42fe12425edafebd7af2299741177d4119
sunil <sunilmaurya@lidolearning.com>
2020-10-22 17:51:02 +0530

    added array question

---
+++ leetcode/array/1480_Running_sum_of_1D_array.cpp
@@ -0,0 +1,55 @@
+// question link
+//https://leetcode.com/problems/running-sum-of-1d-array/
+
+// Given an array nums. We define a running sum of an array as runningSum[i] = sum(nums[0]…nums[i]).
+
+// Return the running sum of nums.
+
+ 
+
+// Example 1:
+
+// Input: nums = [1,2,3,4]
+// Output: [1,3,6,10]
+// Explanation: Running sum is obtained as follows: [1, 1+2, 1+2+3, 1+2+3+4].
+// Example 2:
+
+// Input: nums = [1,1,1,1,1]
+// Output: [1,2,3,4,5]
+// Explanation: Running sum is obtained as follows: [1, 1+1, 1+1+1, 1+1+1+1, 1+1+1+1+1].
+// Example 3:
+
+// Input: nums = [3,1,2,10,1]
+// Output: [3,4,6,16,17]
+
+#include <bits/stdc++.h>
+#define SPEED  ios::sync_with_stdio(false);cin.tie(0); cout.tie(0);
+#define pii pair<int,int>
+#define inf_neg -100000000000000ll
+using namespace std;
+typedef long long int ll;
+ll m=1000000007;
+vector<int> solve(vector<int>&arr)
+{
+    int n=arr.size();
+    int sum=0;
+    for(int i=0;i<n;i++){
+         sum=sum+arr[i];
+         arr[i]=sum;
+    }
+    return arr;
+}
+int main()
+{
+    SPEED;
+    #ifndef ONLINE_JUDGE
+    freopen("i.txt", "r", stdin);   
+    #endif
+    int n;
+    cin>>n;
+    vector<int>arr(n);
+    for(int i=0;i<n;i++)cin>>arr[i];
+    vector<int>ans=solve(arr);
+    for(int i=0;i<n;i++)cout<<ans[i]<<" ";
+    return 0;
+}
