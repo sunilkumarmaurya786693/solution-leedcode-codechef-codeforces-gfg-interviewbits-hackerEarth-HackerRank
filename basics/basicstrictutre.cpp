5fe4d5a1928a9067313c417243e13768f91db8e7
Preeti amurya <preetimaurya1992@gmail.com>
2022-10-20 23:27:51 +0530

    added basic structure of cpp

---
+++ basics/basicstrictutre.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    freopen("i.txt", "r", stdin);
+
+    int n;
+    cin >> n;
+    cout << "Sfds" << n;
+    cout << endl;
+    vector<int>arr={1,2,3};
+    cout<<arr.size();
+
+    return 0;
+}
